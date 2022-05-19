46463fbba46a91e5e5c376be3e511625584fe2c3
jpham005 <jpham005@naver.com>
2022-05-19 21:31:03 +0900

    vector

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "vector_test.hpp"
-#include <vector>
-int main() {
-  int a(5);
-  ~a;
-}
--- vector_test.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "vector.hpp"
-
-bool vector_test() {
-  return true;
-}
